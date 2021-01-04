397e4e18d2ed7448e3de70e92d9db397f70a3248
Harsh Shastri <harshshastri03@gmail.com>
2021-01-04 18:35:21 -0500

    Binary Search Implemented well

---
+++ BinarySearch.py
@@ -0,0 +1,44 @@
+def BinarySearchIterative(arr, value):
+    left : int = 0
+    right : int = len(arr) - 1
+    #print(mid)
+
+    while left <= right:
+        mid: int = (left + right) // 2
+        if value == arr[mid]:
+            print("The value is found at {}".format(mid))
+            return True
+
+        elif value < arr[mid]:
+            right = mid - 1
+
+        elif value > arr[mid]:
+            left = mid + 1
+
+    return False
+
+
+def BinarySearchRecursive(arr, value, left : int, right : int):
+
+    if left > right:
+        return False
+    mid: int = (left + right) // 2
+    if value == arr[mid]:
+        print("The value is found at {}".format(mid))
+        return True
+    elif value < arr[mid]:
+        return BinarySearchRecursive(arr, value, left, mid - 1)
+    elif value > arr[mid]:
+        return BinarySearchRecursive(arr, value, mid + 1, right)
+
+def main():
+    arr = [1,2,6,8,14]
+    BinarySearchIterative(arr, 8)
+    BinarySearchRecursive(arr, 2, 0, len(arr) - 1)
+    BinarySearchIterative(arr, 14)
+    print(BinarySearchRecursive(arr, 13, 0, len(arr)-1))
+    print(BinarySearchRecursive(arr, 14, 0, len(arr) - 1))
+    print(BinarySearchIterative(arr, 14) == BinarySearchRecursive(arr, 14, 0, len(arr) - 1))
+
+if __name__ == '__main__':
+    main()
