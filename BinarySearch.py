aed22e6e0d117460e18d2a5e14189a2a7e489820
Harsh Shastri <harshshastri03@gmail.com>
2021-01-15 01:19:10 -0500

    Binary Search Updated

--- BinarySearch.py
+++ BinarySearch.py
@@ -31,6 +31,21 @@ def BinarySearchRecursive(arr, value, left : int, right : int):
     elif value > arr[mid]:
         return BinarySearchRecursive(arr, value, mid + 1, right)
 
+def BinarySearchIterative2(arr, value, left, right):
+
+    while left <= right:
+        mid: int = (left + right) // 2
+        if value == arr[mid]:
+            return mid
+
+        elif value < arr[mid]:
+            right = mid - 1
+
+        elif value > arr[mid]:
+            left = mid + 1
+
+    return -1 #if not found
+
 def main():
     arr = [1,2,6,8,14]
     BinarySearchIterative(arr, 8)
@@ -39,6 +54,8 @@ def main():
     print(BinarySearchRecursive(arr, 13, 0, len(arr)-1))
     print(BinarySearchRecursive(arr, 14, 0, len(arr) - 1))
     print(BinarySearchIterative(arr, 14) == BinarySearchRecursive(arr, 14, 0, len(arr) - 1))
+    index = BinarySearchIterative2(arr, 8, 0, len(arr) - 1)
+    print(index)
 
 if __name__ == '__main__':
     main()
